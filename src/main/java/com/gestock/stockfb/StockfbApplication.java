b5084b972bce4e0276fe45e219c8f3b064d1663f
ahjoel <aholoujoel5@gmail.com>
2023-06-07 18:15:28 +0000

    initial commit

---
+++ src/main/java/com/gestock/stockfb/StockfbApplication.java
@@ -0,0 +1,13 @@
+package com.gestock.stockfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockfbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestock/stockfb/StockfbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestock.stockfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockfbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
